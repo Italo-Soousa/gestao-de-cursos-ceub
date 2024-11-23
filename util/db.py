c1d1415e72e46e1e4914db437dd1e33e8c507d9d
Italo-Soousa <italomadeira21@gmail.com>
2024-11-23 19:16:45 -0300

    Mudanças feitas para começar

--- util/db.py
+++ util/db.py
@@ -1,2 +1,10 @@
-import mysql.connector 
+import mysql.connector
 
+class SQL:
+    def __init__(self, servidor='localhost', user='root', password='ceub123', esquema='test'):
+        self.conexao = mysql.connector.connect(host=servidor, user=user, password=password, database=esquema)
+
+        cursor = self.conexao.cursor()
+
+        cursor.close()
+        self.conexao.close()
