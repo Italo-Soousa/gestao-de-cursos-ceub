7981cb8fb26a5ca0ad2f3541495f6043e2b0f178
caio0victor <killmobr@gmail.com>
2024-12-11 13:19:49 -0300

    atualição grande editar perfil movido para pasta login para facilitar asseso de outros arquivos .py criação de uma tela de Professor e um arquivo com configuraçoes para a aparencia

--- interface/login/editar_perfil.py
+++ interface/login/editar_perfil.py
@@ -1,52 +1,39 @@
 import tkinter as tk
 from util.db import conexaoBanco
 from tkinter import messagebox
-def Editar_perfil(cor0,cor1, texto, login,cor3,**config_text_box):
-    JGC = tk.Tk()
-    JGC.title(f"{login}")
-    JGC.geometry("600x220")
-    JGC.configure(bg=cor0)
-    JGC.resizable(False, False)
+from util.config import cor0, config_botao, config_text_box, config_text1
 
-    config_text = {
-        'font': ("Arial", 13, "bold"),
-        'fg': texto,
-        'bg': cor0
-    }
-    config_botao = {
-        'font': ("Arial", 10, "bold"),
-        'bg': cor1,
-        'fg': '#DCDCDC',
-        'activebackground': cor3,
-        'relief': "flat",
-    }
-    senha_entry = tk.Entry(JGC,**config_text_box, width=20)
+
+def Editar_perfil(login):
+    JEP = tk.Tk()
+    JEP.title("Editar Perfil")
+    JEP.geometry("600x230")
+    JEP.configure(bg=cor0)
+    JEP.resizable(False, False)
+
+    senha_entry = tk.Entry(JEP, **config_text_box, width=20)
     senha_entry.place(x=20, y=80)
-    email_entry = tk.Entry(JGC,**config_text_box, width=20)
+    email_entry = tk.Entry(JEP, **config_text_box, width=20)
     email_entry.place(x=20, y=140)
     try:
-        # Conecta ao banco usando o util.db
         conexao = conexaoBanco()
         if conexao:
             cursor = conexao.cursor()
 
-            # Consulta as informações do curso pelo login
             cursor.execute("SELECT senha, email, nome FROM perfis WHERE login = %s", (login,))
             resultado = cursor.fetchone()
 
             if resultado:
                 senha, email, nome = resultado
-                print(resultado)
-                # Exibindo o título com o login do curso
-                texto_informativo = tk.Label(JGC, text=f"Nome: {nome}", **config_text)
+
+                texto_informativo = tk.Label(JEP, text=f"Nome do Usuário: {nome}", **config_text1)
                 texto_informativo.place(x=20, y=20)
 
-                # senha
-                texto_senha = tk.Label(JGC, text=f"Senha: {senha}", **config_text)
+                texto_senha = tk.Label(JEP, text=f"Senha do Usuário: {senha}", **config_text1)
                 texto_senha.place(x=20, y=50)
 
-                texto_email  = tk.Label(JGC, text=f"Email: {email}", **config_text)
-                texto_email .place(x=20, y=110)
+                texto_email = tk.Label(JEP, text=f"Email do Usuário: {email}", **config_text1)
+                texto_email.place(x=20, y=110)
 
             cursor.close()
             conexao.close()
@@ -82,7 +69,7 @@ def Editar_perfil(cor0,cor1, texto, login,cor3,**config_text_box):
 
                 # Remove a vírgula final, caso algum campo não tenha sido alterado
                 if update_values:
-                    update_query = update_query.rstrip(", ")  # Remove a vírgula extra no final
+                    update_query = update_query.rstrip(", ")
                     update_query += " WHERE login = %s"
                     update_values.append(login)
                     # Exibindo a consulta para debug
@@ -94,17 +81,17 @@ def Editar_perfil(cor0,cor1, texto, login,cor3,**config_text_box):
                     messagebox.showinfo("Sucesso", "Registro salvo com sucesso!")
                 else:
                     messagebox.showwarning("Aviso", "Nenhum campo foi alterado.")
-                # Fecha a conexão e o cursor
+
                 cursor.close()
                 conexao.close()
 
-                # Fecha a janela de registro
-                JGC.destroy()
+                JEP.destroy()
             else:
                 messagebox.showerror("Erro", "Não foi possível conectar ao banco de dados.")
         except Exception as e:
             messagebox.showerror("Erro", f"Ocorreu um erro ao salvar: {e}")
+
     # Botão para registrar
-    bntDeRegistro = tk.Button(JGC,text="Editar",**config_botao,width= 12,command=salvarDados)
+    bntDeRegistro = tk.Button(JEP, text="Editar", **config_botao, command=salvarDados)
     bntDeRegistro.place(x=475, y=180)
-    JGC.mainloop()
+    JEP.mainloop()
