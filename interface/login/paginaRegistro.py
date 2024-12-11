324b738a5310a15498acab2158ff8c3b3a6f8d38
caio0victor <killmobr@gmail.com>
2024-12-11 13:54:12 -0300

    mudança na parencia da tela de registro

--- interface/login/paginaRegistro.py
+++ interface/login/paginaRegistro.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 from util.db import conexaoBanco  # Importa a função de conexão
-
+from util.config import cor0, config_botao, config_text1, config_text_box
 
 def abrirPaginaRegistro():
 
@@ -11,36 +11,37 @@ def abrirPaginaRegistro():
     pagina_registro = tk.Toplevel()
     pagina_registro.title("Página de Registro")
     pagina_registro.geometry("300x430")
+    pagina_registro.configure(bg=cor0)
     pagina_registro.resizable(False, False)
 
     # Label "Nome"
-    nome = tk.Label(pagina_registro, text="NOME", font=("Arial", 10, "bold"))
+    nome = tk.Label(pagina_registro, text="NOME",**config_text1)
     nome.pack(pady=(10, 5))
-    PegandoNome = tk.Entry(pagina_registro, width=25, justify="center", font=("Arial", 12))
+    PegandoNome = tk.Entry(pagina_registro, width=25,**config_text_box)
     PegandoNome.pack(pady=(0, 5))
 
     # Label "E-mail"
-    email = tk.Label(pagina_registro, text="EMAIL", font=("Arial", 10, "bold"))
+    email = tk.Label(pagina_registro, text="EMAIL",**config_text1)
     email.pack(pady=(10, 5))
-    PegandoEmail = tk.Entry(pagina_registro, width=25, justify="center", font=("Arial", 12))
+    PegandoEmail = tk.Entry(pagina_registro, width=25,**config_text_box)
     PegandoEmail.pack(pady=(0, 5))
 
     # Label "Login"
-    login = tk.Label(pagina_registro, text="LOGIN", font=("Arial", 10, "bold"))
+    login = tk.Label(pagina_registro, text="LOGIN",**config_text1)
     login.pack(pady=(10, 5))
-    PegandoLogin = tk.Entry(pagina_registro, width=25, justify="center", font=("Arial", 12))
+    PegandoLogin = tk.Entry(pagina_registro, width=25,**config_text_box)
     PegandoLogin.pack(pady=(0, 15))
 
     # Label "Senha"
-    senha = tk.Label(pagina_registro, text="SENHA", font=("Arial", 10, "bold"))
+    senha = tk.Label(pagina_registro, text="SENHA", **config_text1)
     senha.pack(pady=(10, 5))
-    PegandoSenha = tk.Entry(pagina_registro, width=25, justify="center", font=("Arial", 12), show="*")
+    PegandoSenha = tk.Entry(pagina_registro, width=25,**config_text_box, show="*")
     PegandoSenha.pack(pady=(0, 20))
 
     # Label "Tipo de Usuário"
-    senha = tk.Label(pagina_registro, text="TIPO USUÁRIO", font=("Arial", 10, "bold"))
+    senha = tk.Label(pagina_registro, text="TIPO USUÁRIO", **config_text1)
     senha.pack(pady=(10, 5))
-    PaginaTipoUsuario = tk.Entry(pagina_registro, width=25, justify="center", font=("Arial", 12))
+    PaginaTipoUsuario = tk.Entry(pagina_registro, width=25,**config_text_box)
     PaginaTipoUsuario.pack(pady=(0, 20))
 
     # Função para salvar os dados no banco de dados
@@ -85,5 +86,5 @@ def abrirPaginaRegistro():
             messagebox.showerror("Erro", f"Ocorreu um erro ao salvar: {e}")
 
     # Botão para registrar
-    bntDeRegistro = tk.Button(pagina_registro,text="Registrar", width=12, font=("Arial", 10, "bold"), command=salvarDados)
-    bntDeRegistro.pack(pady=(10, 20))
+    btn_registro = tk.Button(pagina_registro, text="Registrar", **config_botao, command=salvarDados)
+    btn_registro.place(x=100, y=375)
